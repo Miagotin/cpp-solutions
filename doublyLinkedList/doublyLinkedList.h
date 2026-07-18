b6e537c0d8533da71bf0bc1ec4185a30126d48df
Aleksei <alekseimiagotin@gmail.com>
2026-07-18 22:36:24 +0300

    Create doublyLinkedList.h

---
+++ doublyLinkedList/doublyLinkedList.h
@@ -0,0 +1,151 @@
+#ifndef DOUBLY_LINKED_LIST_H
+#define DOUBLY_LINKED_LIST_H
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+template <typename T>
+struct Node {
+    T value;
+    Node* prev;
+    Node* next;
+};
+
+template <typename T>
+struct LinkedList {
+    Node<T>* head;
+    Node<T>* tail;
+    size_t size;
+    
+    LinkedList() : head(nullptr), tail(nullptr), size(0) {}
+    
+    LinkedList(initializer_list<T> init) : head(nullptr), tail(nullptr), size(0) {
+        for(auto value : init) {
+            this->push_back(value);
+        }
+    }
+    
+    LinkedList(const LinkedList& other) : head(nullptr), tail(nullptr), size(0) {
+        Node<T>* current = other.head;
+        while(current != nullptr) {
+            this->push_back(current->value);
+            current = current-> next;
+        }
+    }
+    
+    ~LinkedList() {
+        Node<T>* current = head;
+        while(current != nullptr) {
+            Node<T>* next = current-> next;
+            delete current;
+            currnet = next;
+        }
+    }
+    
+    void push_back(T value) {
+
+        if(size == 0) {
+            Node<T>* newNode = new Node<T>{value, nullptr, nullptr};
+            
+            head = tail = newNode;
+        }
+        else {
+            Node<T>* newNode = new Node<T>{value, tail, nullptr};
+            
+            tail->next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+    
+    void push_front(T value) {
+        
+        if(size == 0) {
+            Node<T>* newNode = new Node<T>{value, nullptr, nullptr};
+            
+            head = tail = newNode;
+        }
+        else {
+            Node<T>* newNode = new Node<T>{value, nullptr, head};
+            
+            head->prev = newNode;
+            head = newNode;
+        }
+        size++;
+    }
+    
+    void pop_back() {
+        
+        if(size == 0) {
+            return;
+        }
+        
+        Node<T>* prevTail = tail;
+        
+        if(size == 1) {
+            head = tail = nullptr;
+        }
+        else {
+            tail = tail->prev;
+            tail->next = nullptr;
+        }
+        
+        delete prevTail;
+        size--;
+    }
+    
+    void pop_front() {
+        
+        if(size == 0) {
+            return;
+        }
+        
+        Node<T>* prevHead = head;
+        
+        if(size == 1) {
+            head = tail = nullptr;
+        }
+        else {
+            head = head->next;
+            head->prev = nullptr;
+        }
+        
+        delete prevHead;
+        size--;
+    }
+    
+    void clear() {
+        Node<T>* current = head;
+        while(current != nullptr) {
+            Node<T>* next = current->next;
+            delete current;
+            current = next;
+        }
+        head = tail = nullptr;
+        size = 0;
+    }
+    
+    size_t getSize() const {
+        return size;
+    }
+    
+    bool empty() const {
+        return !size;
+    }
+    
+    void print() const {
+        Node<T>* current = head;
+        while(current != nullptr) {
+            cout << current->value;
+            if(current->next != nullptr) {
+                cout << ' ';
+            }
+            current = current->next;
+        }
+        cout << endl;
+    }
+    
+};
+
+#endif // DOUBLY_LINKED_LIST_H
